e696c35cb9cb2553abc68232a7066661c5dbcee9
Pedro Pereira Guaita <pedropguaita@gmail.com>
2025-07-28 19:02:02 -0300

    Add files via upload

---
+++ OperadoresAritimeticos/OperadoresAritimeticos.java
@@ -0,0 +1,30 @@
+public class OperadoresAritimeticos {
+    public static void main(String[] args) {
+        /*
+        int n1 = 3;
+        int n2 = 5;
+        float media = (n1 + n2)/2;
+        System.out.printf("A média é igual a %.2f", media);
+        */
+
+        int numero = 5;
+        numero++;
+        int valor = 5 + ++numero; //int valor = 5 + numero++;
+
+        //arredondamentos
+
+        float v = 8.3f;
+        //int ar = (int) Math.floor(v);  //floor arredonda para baixo
+        //int ar = (int) Math.ceil(v) //ceil arredonda para cima
+        int ar = (int) Math.round(v);
+        System.out.println(ar);
+
+        //gerador de numeros
+
+        //math.random()
+
+        double aleatorio = Math.random();
+        int n = (int) (15 + aleatorio * (50 - 15));
+        System.out.println(aleatorio);
+    }
+}
