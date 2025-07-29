6b9aa29f394b422f94ef698b35e490bb1cd97ab4
Pedro Pereira Guaita <pedropguaita@gmail.com>
2025-07-29 17:49:02 -0300

    Update EntradaDeDados.java

--- EntradaDeDados/EntradaDeDados.java
+++ EntradaDeDados/EntradaDeDados.java
@@ -20,7 +20,7 @@ public class EntradaDeDados {
 
         System.out.printf("Você digitou: %.2f", z);       //println sempre usa . (ponto), ja o printf nao.
         System.out.println("\nVoce digitou: " + a);
-        */
+        
 
         //LER VARIOS DADOS NA MESMA LINHA, ou seja, separados por espaço
 
@@ -36,8 +36,23 @@ public class EntradaDeDados {
         System.out.println(x);
         System.out.println(y);
         System.out.println(z);
-
-
+        */
+        
+        //LER UM TEXTO ATÉ QUEBRA DE LINHA
+        int x;
+        String s1, s2, s3;
+        
+        x = entrada.nextInt();       //é necessario um nextLine() para "consumir" a quebra de linha
+        entrada.nextLine();
+        s1 = entrada.nextLine();    //para ler a linha inteira precisa usar o Line.
+        s2 = entrada.nextLine();
+        s3 = entrada.nextLine();
+        
+        System.out.println("DADOS DIGITADOS");
+        System.out.println("s1");
+        System.out.println("s2");
+        System.out.println("s3");
+        
         entrada.close();
     }
 }
