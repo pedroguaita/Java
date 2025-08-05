e34215906964d03aafbe31ad9b5714fbf397a038
Pedro Pereira Guaita <pedropguaita@gmail.com>
2025-08-04 21:52:41 -0300

    Update Triangle.java

--- Programação Orientada a Objetos/Introdução/Triangle.java
+++ Programação Orientada a Objetos/Introdução/Triangle.java
@@ -1,7 +1,13 @@
 package entities;
-
+//CLASSE
 public class Triangle {
+//ATRIBUTOS
     public double a;
     public double b;
     public double c;
+//MÉTODOS
+    public double area(){
+        double p = (a + b + c) / 2.0;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
 }
