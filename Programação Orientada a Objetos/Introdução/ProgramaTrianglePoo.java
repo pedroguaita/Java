3cbda480424411c6d400ca3aa570add4b811b821
Pedro Pereira Guaita <pedropguaita@gmail.com>
2025-08-04 21:50:11 -0300

    Update ProgramaTrianglePoo.java

--- Programação Orientada a Objetos/Introdução/ProgramaTrianglePoo.java
+++ Programação Orientada a Objetos/Introdução/ProgramaTrianglePoo.java
@@ -23,11 +23,8 @@ public class ProgramaTrianglePoo {
         y.b = sc.nextDouble();
         y.c = sc.nextDouble();
 
-        double p = (x.a + x.b + x.c) / 2.0;
-        areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
-
-        p = (y.a + y.b + y.c) / 2.0;
-        areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+        areaX = x.area();
+        areaY = y.area();
 
         System.out.printf("Area triangulo X: %.4f\n", areaX);
         System.out.printf("Area triangulo Y: %.4f\n", areaY);
@@ -40,3 +37,4 @@ public class ProgramaTrianglePoo {
         }
     }
 }
+
