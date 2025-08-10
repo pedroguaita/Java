3a3e50a3d04aa3aa041cedaa859a3500fa1b0d09
Pedro Pereira Guaita <pedropguaita@gmail.com>
2025-08-09 22:46:20 -0300

    Update ProgramaProdutos.java

--- Programação Orientada a Objetos/Introdução/ProgramaProdutos.java
+++ Programação Orientada a Objetos/Introdução/ProgramaProdutos.java
@@ -21,8 +21,22 @@ public class ProgramaProdutos {
         System.out.print("Quantidade em estoque: ");
         produto.quantidade = sc.nextInt();
 
-        //System.out.printf("Informações digitadas: %s, R$%.2f, %d unidades", produto.nome, produto.preco, produto.quantidade);
+        //System.out.printf("Informações digitdas: %s, R$%.2f, %d unidades", produto.nome, produto.preco, produto.quantidade);
         System.out.print("Informações digitadas: " + produto);  //-> System.out.println(produto.toString());
+
+        System.out.println("\n\nEntre com a quantidade do produto a ser adicionado ao estoque");
+        int quantity = sc.nextInt();
+        produto.addProdutos(quantity);
+
+        System.out.println();
+        System.out.print("Informações atualizadas: " + produto);
+
+        System.out.println("\n\nEntre com a quantidade do produto a ser removido ao estoque");
+        quantity = sc.nextInt();
+        produto.removerProdutos(quantity);
+
+        System.out.println();
+        System.out.print("Informações atualizadas: " + produto);
 
         sc.close();
     }
