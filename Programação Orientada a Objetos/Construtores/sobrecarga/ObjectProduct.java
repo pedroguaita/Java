8faf8ad5cf492fee50bd452b892b5da5e16b148f
Pedro Pereira Guaita <pedropguaita@gmail.com>
2025-08-25 20:49:51 -0300

    Create ObjectProduct.java

---
+++ Programação Orientada a Objetos/Construtores/sobrecarga/ObjectProduct.java
@@ -0,0 +1,47 @@
+package construtores.objects;
+
+public class ObjectProduct {
+
+        //ATRIBUTOS
+        public String nome;
+        public double preco;
+        public int quantidade;
+
+        //Construtor
+        public ObjectProduct(String nome, double preco, int quantidade){
+            this.nome = nome;
+            this.preco = preco;
+            this.quantidade = quantidade;
+        }
+
+        //Construtor (Sobrecarga)
+        public ObjectProduct(String nome, double preco){
+            this.nome = nome;
+            this.preco = preco;
+            //quantidade = 0;
+        }
+
+        //MÉTODOS
+        public double totalValorEmEstoque(){
+            return preco * quantidade;
+        }
+
+        public void addProdutos(int quantidade){
+            this.quantidade += quantidade;
+        }
+
+        public void removerProdutos(int quantidade){
+            this.quantidade -= quantidade;
+        }
+
+        public String toString(){
+            return nome
+                    + ", $ "
+                    + String.format("%.2f", preco)
+                    + ", "
+                    + quantidade
+                    + " unidades, Total: R$ "
+                    + String.format("%.2f", totalValorEmEstoque());
+        }
+    }
+
