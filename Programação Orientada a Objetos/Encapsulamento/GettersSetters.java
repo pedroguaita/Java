fe96a7a25896390e2a8ae662c861bac154c87a49
Pedro Pereira Guaita <pedropguaita@gmail.com>
2025-08-26 19:53:24 -0300

    Add files via upload

---
+++ Programação Orientada a Objetos/Encapsulamento/GettersSetters.java
@@ -0,0 +1,22 @@
+package encapsulamento.objetcs;
+
+public class GettersSetters {
+    private String name;
+    private double price;
+
+    //obter o atributo
+    public String getName() {
+        return name;
+    }
+
+    //alterar o atributo
+    public void setName(String name) {
+        this.name = name;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
