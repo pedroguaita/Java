0fba06c888479d64adba9e2025bfc6ce4cf0ea90
Pedro Pereira Guaita <pedropguaita@gmail.com>
2025-08-28 00:39:57 -0300

    Update Program1.java

--- Programação Orientada a Objetos/exercícios/main/Program1.java
+++ Programação Orientada a Objetos/exercícios/main/Program1.java
@@ -3,14 +3,18 @@ package exercicios9.main;
 import exercicios9.entities.Account;
 import java.util.Scanner;
 
+import static java.lang.System.exit;
+
 public class Program1 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
+        //VARIABLES
+        Account account;
         int number;
         String holder, answer;
-        double balance;
+        double initialDeposit, depositValue, withdrawValue;
 
         System.out.println("---------- Welcome! ----------");
 
@@ -22,29 +26,38 @@ public class Program1 {
         holder = sc.nextLine();
 
         System.out.print("Is there an initial deposit(yes/no)? ");
-        answer = sc.nextLine();
+        answer = sc.nextLine();   //char response = sc.next().charAt(0); -> caso usasse (y/n).
 
         answer = answer.trim().toLowerCase();
         if(answer.equals("yes") || answer.equals("y")){
             System.out.print("Enter your initial deposit value: ");
-            balance = sc.nextDouble();
+            initialDeposit = sc.nextDouble();
             sc.nextLine();
 
-            Account account = new Account(number, holder, balance);
-
-            System.out.println("\n---------- Account data ---------- ");
-            System.out.println(account.toString());
-        }
-        else if(answer.equals("no") || answer.equals("n")){
-            Account account = new Account(number, holder);
-
-            System.out.println("\n---------- Account data ---------- ");
-            System.out.println(account.toString());
+            account = new Account(number, holder, initialDeposit);
         }
         else{
-            System.out.println("Try again...");
+            account = new Account(number, holder);
         }
+
+        System.out.println("\n---------- Account data ---------- ");
+        System.out.println(account.toString());
+
+        System.out.print("\nEnter a deposit value: ");
+        depositValue = sc.nextDouble();
+        account.deposit(depositValue);
+
+        System.out.println("\n---------- Updated account data ---------- ");
+        System.out.println(account);
+
+        System.out.print("\nEnter a withdraw value: ");
+        withdrawValue = sc.nextDouble();
+        account.withdraw(withdrawValue);
+
+        System.out.println("\n---------- Updated account data ---------- ");
+        System.out.println(account);
 
         sc.close();
     }
+
 }
